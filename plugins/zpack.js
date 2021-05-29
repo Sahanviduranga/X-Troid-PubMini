86347d129ed2ba422d978b689701952ac6e1fd95
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-05-29 13:25:46 +0530

    Update zpack.js

--- plugins/zpack.js
+++ plugins/zpack.js
@@ -15,13 +15,14 @@ const cwebp = require('cwebp-bin');
 const Config = require('../config');
 
 const Language = require('../language');
-const Lang = Language.getString('zpack');
+const dd = "To make awesome logos"
+const ddd = "ඔබට විවිධාකාරයේ ලෝගෝ සෑදීමට"
 
 
 if (Config.WORKTYPE == 'private') {
   if (Config.LANG == 'EN') {
 
-    Asena.addCommand({pattern: 'zpack', fromMe: true,}, (async (message, match) => {    
+    Asena.addCommand({pattern: 'zpack', fromMe: true, desc: dd }, (async (message, match) => {    
 
                 await message.sendMessage('╔════════════════════╗\n  ✥▬ *❄️The ZPack❄️* ▬✥\n\n\n🧪 command👉🏻 *.zbt*\n📃 guide👉🏻Make battlefield 4 logo\n\n🧪 command👉 *.zcf*\n📃 guide👉🏻 Make hart coffee cup logo\n\n🧪 command👉 *.zccf*\n📃 guide👉 Make coffee cup logo\n\n🧪 command👉🏻 *.zlogo*\n📃 guide👉🏻 Make green black logo\n\n🧪 command👉🏻 *.zsky*\n📃 guide👉🏻 make sky shadow logo\n\n🧪 command👉🏻 *.zglow*\n📃 guide👉🏻 Make neone colour logo\n\n🧪 command👉🏻 *.zgglow*\n📃 guide👉🏻 Make stylish neone logo\n\n🧪 command👉🏻 *.zharry*\n📃 guide👉🏻 Make harry potter efect logo\n\n🧪 command👉🏻 *.zlove*\n📃 guide👉🏻 Make lovely name card\n\n🧪 command👉🏻 *.zmug*\n📃 guide👉🏻 Make name printed mug logo\n\n🧪 command👉🏻 *.zpep*\n📃 guide👉🏻 Make paer strip on grass logo\n\n🧪 command👉🏻 *.zpubg*\n📃 guide👉🏻 Make pubg mobile logo\n\n🧪 command👉🏻 *.zttok*\n📃 guide👉🏻 Make tiktok efect logo\n\n🧪 command👉🏻 *.zwood*\n📃 guide👉🏻 Make name on wood block logo\n\n🧪 command👉🏻 *.zphub*\n📃 guide👉🏻 Make pornhub style Logo');
 
@@ -29,7 +30,7 @@ if (Config.WORKTYPE == 'private') {
   }
     if (Config.LANG == 'SI') {
 
-    Asena.addCommand({pattern: 'zpack', fromMe: true, }, (async (message, match) => {    
+    Asena.addCommand({pattern: 'zpack', fromMe: true, desc: ddd }, (async (message, match) => {    
 
                 await message.sendMessage('╔════════════════════╗\n  ✥▬ *❄️The ZPack❄️* ▬✥\n\n\n🧪 command👉🏻 *.zbt*\n📃 guide👉🏻 battlefield 4 logo එකක් හදාගන්න\n\n🧪 command👉 *.zcf*\n📃 guide👉🏻 hart coffee cup logo එකක් හදාගන්න\n\n🧪 command👉 *.zccf*\n📃 guide👉 coffee cup logoඑකක් හදාගන්න\n\n🧪 command👉🏻 *.zlogo*\n📃 guide👉green black logo එකක් හදාගන්න\n\n🧪 command👉🏻 *.zsky*\n📃 guide👉� sky shadow logo එකක් හදාගන්න \n\n🧪 command👉🏻 *.zglow*\n📃 guide👉🏻 neone colour logo එකක් හදාගන්න\n\n🧪 command👉🏻 *.zgglow*\n📃 guide👉🏻stylish neone logo එකක් හදාගන්න\n\n🧪 command👉🏻 *.zharry*\n📃 guide👉🏻harry potter efect logo එකක් හදාගන්න\n\n🧪 command👉🏻 *.zlove*\n📃 guide👉🏻lovely name card එකක් හදාගන්න\n\n🧪 command👉🏻 *.zmug*\n📃 guide👉🏻 name printed mug logo එකක් හදාගන්න\n\n🧪 command👉🏻 *.zpep*\n📃 guide👉🏻 paer strip on grass logo  එකක් හදාගන්න\n\n🧪 command👉🏻 *.zpubg*\n📃 guide👉🏻 pubg mobile logo එකක් හදාගන්න\n\n🧪 command👉🏻 *.zttok*\n📃 guide👉🏻 tiktok efect logo එකක් හදාගන්න\n\n🧪 command👉🏻 *.zwood*\n📃 guide👉🏻 name on wood block logo එකක් හදාගන්න\n\n🧪 command👉🏻 *.zphub*\n📃 guide👉🏻 pornhub style Logo එකක් හදාගන්න');
 
@@ -39,7 +40,7 @@ if (Config.WORKTYPE == 'private') {
 else if (Config.WORKTYPE == 'public') {
   if (Config.LANG == 'EN') {
 
-    Asena.addCommand({pattern: 'zpack', fromMe: false,}, (async (message, match) => {    
+    Asena.addCommand({pattern: 'zpack', fromMe: false, desc: dd}, (async (message, match) => {    
 
                 await message.sendMessage('╔════════════════════╗\n  ✥▬ *❄️The ZPack❄️* ▬✥\n\n\n🧪 command👉🏻 *.zbt*\n📃 guide👉🏻Make battlefield 4 logo\n\n🧪 command👉 *.zcf*\n📃 guide👉🏻 Make hart coffee cup logo\n\n🧪 command👉 *.zccf*\n📃 guide👉 Make coffee cup logo\n\n🧪 command👉🏻 *.zlogo*\n📃 guide👉🏻 Make green black logo\n\n🧪 command👉🏻 *.zsky*\n📃 guide👉🏻 make sky shadow logo\n\n🧪 command👉🏻 *.zglow*\n📃 guide👉🏻 Make neone colour logo\n\n🧪 command👉🏻 *.zgglow*\n📃 guide👉🏻 Make stylish neone logo\n\n🧪 command👉🏻 *.zharry*\n📃 guide👉🏻 Make harry potter efect logo\n\n🧪 command👉🏻 *.zlove*\n📃 guide👉🏻 Make lovely name card\n\n🧪 command👉🏻 *.zmug*\n📃 guide👉🏻 Make name printed mug logo\n\n🧪 command👉🏻 *.zpep*\n📃 guide👉🏻 Make paer strip on grass logo\n\n🧪 command👉🏻 *.zpubg*\n📃 guide👉🏻 Make pubg mobile logo\n\n🧪 command👉🏻 *.zttok*\n📃 guide👉🏻 Make tiktok efect logo\n\n🧪 command👉🏻 *.zwood*\n📃 guide👉🏻 Make name on wood block logo\n\n🧪 command👉🏻 *.zphub*\n📃 guide👉🏻 Make pornhub style Logo');
 
@@ -47,7 +48,7 @@ else if (Config.WORKTYPE == 'public') {
   }
     if (Config.LANG == 'SI') {
 
-    Asena.addCommand({pattern: 'zpack', fromMe: false, }, (async (message, match) => {    
+    Asena.addCommand({pattern: 'zpack', fromMe: false, desc: ddd }, (async (message, match) => {    
 
                 await message.sendMessage('╔════════════════════╗\n  ✥▬ *❄️The ZPack❄️* ▬✥\n\n\n🧪 command👉🏻 *.zbt*\n📃 guide👉🏻 battlefield 4 logo එකක් හදාගන්න\n\n🧪 command👉 *.zcf*\n📃 guide👉🏻 hart coffee cup logo එකක් හදාගන්න\n\n🧪 command👉 *.zccf*\n📃 guide👉 coffee cup logoඑකක් හදාගන්න\n\n🧪 command👉🏻 *.zlogo*\n📃 guide👉green black logo එකක් හදාගන්න\n\n🧪 command👉🏻 *.zsky*\n📃 guide👉� sky shadow logo එකක් හදාගන්න \n\n🧪 command👉🏻 *.zglow*\n📃 guide👉🏻 neone colour logo එකක් හදාගන්න\n\n🧪 command👉🏻 *.zgglow*\n📃 guide👉🏻stylish neone logo එකක් හදාගන්න\n\n🧪 command👉🏻 *.zharry*\n📃 guide👉🏻harry potter efect logo එකක් හදාගන්න\n\n🧪 command👉🏻 *.zlove*\n📃 guide👉🏻lovely name card එකක් හදාගන්න\n\n🧪 command👉🏻 *.zmug*\n📃 guide👉🏻 name printed mug logo එකක් හදාගන්න\n\n🧪 command👉🏻 *.zpep*\n📃 guide👉🏻 paer strip on grass logo  එකක් හදාගන්න\n\n🧪 command👉🏻 *.zpubg*\n📃 guide👉🏻 pubg mobile logo එකක් හදාගන්න\n\n🧪 command👉🏻 *.zttok*\n📃 guide👉🏻 tiktok efect logo එකක් හදාගන්න\n\n🧪 command👉🏻 *.zwood*\n📃 guide👉🏻 name on wood block logo එකක් හදාගන්න\n\n🧪 command👉🏻 *.zphub*\n📃 guide👉🏻 pornhub style Logo එකක් හදාගන්න');
 
