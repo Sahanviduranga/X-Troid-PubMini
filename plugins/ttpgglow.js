9dda5ca915dc3f5e136fe78715e834f3cbd26995
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-06-02 22:01:10 +0530

    Update ttpgglow.js

--- plugins/ttpgglow.js
+++ plugins/ttpgglow.js
@@ -1,3 +1,5 @@
+
+  
 /* # Exclusively from danuma project 
 # Do not use this fore any commercial thing
 # If you abuse thais bot we wil kick you from bot 
@@ -16,11 +18,11 @@ const Api = (Config.LOL_API_KEY)
 
 if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({ pattern: 'zgglow ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+    Asena.addCommand({ pattern: 'zgglow ?(.*)', fromMe: true, deleteCommand: false, }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage();
 
-        var ttinullimage = await axios.get(`https://api.lolhuman.xyz/api/textprome/greenneon?apikey=${Api}&text=${match[1].replace(/Ã–/g, "%C3%96").replace(/Ã¶/g, "%C3%B6").replace(/Ã¼/g, "%C3%BC").replace(/Ãœ/g, "%C3%9C").replace(/Äž/g, "%C4%9E").replace(/ÄŸ/g, "%C4%9F").replace(/ÅŸ/g, "%C5%9F").replace(/Åž/g, "%C5%9E").replace(/Ã§/g, "%C3%A7").replace(/Ã‡/g, "%C3%87").replace(/Ä±/g, "%C4%B1").replace(/i/g, "%69").replace(/"/g, "%22").replace(/Ä°/g, "%C4%B0")}`, { responseType: 'arraybuffer' })
+        var ttinullimage = await axios.get(`https://api.lolhuman.xyz/api/textprome/greenneon?apikey=${Api}&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
         await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '🚀Made by X-Troid ☄️' })
 
@@ -29,11 +31,11 @@ if (Config.WORKTYPE == 'private') {
 
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({ pattern: 'zgglow ?(.*)', fromMe: false,deleteCommand: false,dontAddCommandList: true }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'zgglow ?(.*)', fromMe: false,deleteCommand: false, }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage();
 
-        var ttinullimage = await axios.get(`https://api.lolhuman.xyz/api/textprome/greenneon?apikey=${Api}&text=${match[1].replace(/Ã–/g, "%C3%96").replace(/Ã¶/g, "%C3%B6").replace(/Ã¼/g, "%C3%BC").replace(/Ãœ/g, "%C3%9C").replace(/Äž/g, "%C4%9E").replace(/ÄŸ/g, "%C4%9F").replace(/ÅŸ/g, "%C5%9F").replace(/Åž/g, "%C5%9E").replace(/Ã§/g, "%C3%A7").replace(/Ã‡/g, "%C3%87").replace(/Ä±/g, "%C4%B1").replace(/i/g, "%69").replace(/"/g, "%22").replace(/Ä°/g, "%C4%B0")}`, { responseType: 'arraybuffer' })
+        var ttinullimage = await axios.get(`https://api.lolhuman.xyz/api/textprome/greenneon?apikey=${Api}&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
         await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '🚀Made by X-Troid ☄️' })
 
