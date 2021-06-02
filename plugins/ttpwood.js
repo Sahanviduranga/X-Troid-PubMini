867f9cd311fe13a4c18f92ce684de16a4ceefa72
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-06-02 10:17:35 +0530

    Update ttpwood.js

--- plugins/ttpwood.js
+++ plugins/ttpwood.js
@@ -17,17 +17,6 @@ const Lang = Language.getString('ttp');
 
 
 if (Config.WORKTYPE == 'private') {
-Asena.addCommand({ pattern: 'kk ?(.*)', fromMe: true, deleteCommand: false, desc: Lang.TTOK_DESC }, (async (message, match) => {
-
-    if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
-
-    await axios.get(`https://ttpxc.herokuapp.com/api/textmaker?text=${match[1].replace(/Ã–/g, "%C3%96").replace(/Ã¶/g, "%C3%B6").replace(/Ã¼/g, "%C3%BC").replace(/Ãœ/g, "%C3%9C").replace(/Äž/g, "%C4%9E").replace(/ÄŸ/g, "%C4%9F").replace(/ÅŸ/g, "%C5%9F").replace(/Åž/g, "%C5%9E").replace(/Ã§/g, "%C3%A7").replace(/Ã‡/g, "%C3%87").replace(/Ä±/g, "%C4%B1").replace(/i/g, "%69").replace(/"/g, "%22").replace(/Ä°/g, "%C4%B0")}`).then(async (res) => {
-
-        await message.sendMessage({ url: res.data.results}, MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by asena'})
-
-    })
-
-}));
 }
 
 else if (Config.WORKTYPE == 'public') {
