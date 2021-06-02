cc589e21eab586ccaef4cb5902611b0e0e281a6f
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-06-02 21:33:21 +0530

    Update grp.js

--- plugins/grp.js
+++ plugins/grp.js
@@ -1,8 +1,9 @@
 const Asena = require('../events');
-
+const Config = require('../config');
 const {MessageType} = require('@adiwajshing/baileys');
-
 const Language = require('../language');
+const Lang = Language.getString('_asena');
+
 
 
 if (Config.WORKTYPE == 'private') {
